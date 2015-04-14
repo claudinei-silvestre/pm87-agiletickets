e2b93ccd7293a1e4526cd25855e53e2b95e84d75
¨magno¨ <is.magnojr@gmail.com>
2015-04-14 12:07:03 -0300

    correcao na quantidade de tickets que podem ser reservados para uma sessao

--- src/test/java/br/com/caelum/agiletickets/models/SessaoTest.java
+++ src/test/java/br/com/caelum/agiletickets/models/SessaoTest.java
@@ -1,14 +1,12 @@
 package br.com.caelum.agiletickets.models;
 
-import javax.validation.constraints.AssertTrue;
-
 import org.junit.Assert;
 import org.junit.Test;
 
 public class SessaoTest {
 
 	@Test
-	public void deveVender1ingressoSeHa2vagas() throws Exception {
+	public void deveVenderingressoSeHavagasSobrando() throws Exception {
 		Sessao sessao = new Sessao();
         sessao.setTotalIngressos(2);
 
@@ -16,22 +14,16 @@ public class SessaoTest {
 	}
 	
 	@Test
-	public void deveVender2IngressosSeHaVagas(){
+	public void deveVenderIngressosSeONunemroDeVagasSobrandoEIgualAQuantidadeDeIngressosVendido(){
 		Sessao sessao = new Sessao();
 		sessao.setTotalIngressos(2);
 		Assert.assertTrue(sessao.podeReservar(2));
 	}
 	
-	@Test
-	public void deveVender5ingressosSeHa10vagas() throws Exception {
-		Sessao sessao = new Sessao();
-		sessao.setTotalIngressos(10);
-		
-		Assert.assertTrue(sessao.podeReservar(5));
-	}
+
 
 	@Test
-	public void naoDeveVender3ingressoSeHa2vagas() throws Exception {
+	public void naoDeveVenderingressoSeNaoHaQuantidadeDeVagasDisponiveis() throws Exception {
 		Sessao sessao = new Sessao();
 		sessao.setTotalIngressos(2);
 
