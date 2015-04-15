4f73a598dccf13643810feb413f058083a8452f0
¨magno¨ <is.magnojr@gmail.com>
2015-04-15 10:19:29 -0300

    correca na calculadora de preco - mudanca 4

--- src/main/java/br/com/caelum/agiletickets/domain/precos/CalculadoraDePrecos.java
+++ src/main/java/br/com/caelum/agiletickets/domain/precos/CalculadoraDePrecos.java
@@ -23,24 +23,9 @@ public class CalculadoraDePrecos {
 		if(espetaculo.getTipo().equals(TipoDeEspetaculo.CINEMA) || espetaculo.getTipo().equals(TipoDeEspetaculo.SHOW)) {
 			preco = calculaPrecoCinemaEShow(sessao);
 		} else if(espetaculo.getTipo().equals(TipoDeEspetaculo.BALLET) || espetaculo.getTipo().equals(TipoDeEspetaculo.ORQUESTRA)) {
-			preco = calculaPrecoBalletEOrquestra(sessao);
+			preco = Sessao.calculaPrecoBalletEOrquestra(sessao);
 		}  else {
-			//nao aplica aumento para teatro (quem vai é pobretão)
 			preco = sessao.getPreco();
-		}
-		return preco;
-	}
-
-	private static BigDecimal calculaPrecoBalletEOrquestra(Sessao sessao) {
-		BigDecimal preco;
-		if((sessao.getTotalIngressos() - sessao.getIngressosReservados()) / sessao.getTotalIngressos().doubleValue() <= 0.50) { 
-			preco = sessao.getPreco().add(sessao.getPreco().multiply(BigDecimal.valueOf(0.20)));
-		} else {
-			preco = sessao.getPreco();
-		}
-		
-		if(sessao.getDuracaoEmMinutos() > 60){
-			preco = preco.add(sessao.getPreco().multiply(BigDecimal.valueOf(0.10)));
 		}
 		return preco;
 	}
--- src/main/java/br/com/caelum/agiletickets/models/Sessao.java
+++ src/main/java/br/com/caelum/agiletickets/models/Sessao.java
@@ -113,4 +113,18 @@ public class Sessao {
 		return preco;
 	}
 
+	public static BigDecimal calculaPrecoBalletEOrquestra(Sessao sessao) {
+		BigDecimal preco;
+		if((sessao.getTotalIngressos() - sessao.getIngressosReservados()) / sessao.getTotalIngressos().doubleValue() <= 0.50) { 
+			preco = sessao.getPreco().add(sessao.getPreco().multiply(BigDecimal.valueOf(0.20)));
+		} else {
+			preco = sessao.getPreco();
+		}
+		
+		if(sessao.getDuracaoEmMinutos() > 60){
+			preco = preco.add(sessao.getPreco().multiply(BigDecimal.valueOf(0.10)));
+		}
+		return preco;
+	}
+
 }
